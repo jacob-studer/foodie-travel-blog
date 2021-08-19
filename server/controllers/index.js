edb24118d2e69152f92b930c7f6c0a14de0a3ee1
Jake Studer <jacob.studer.dev@gmail.com>
2021-08-18 22:45:46 -0600

    added server config file, started on controllers, intial model schemas, started routing page, add auth util

---
+++ server/controllers/index.js
@@ -0,0 +1,32 @@
+const { User } = require('../models');
+
+const { signToken } = require('../utils/auth');
+
+module.exports = {
+    async getSingleUser({ user = null, params }, res) {
+        const foundUser = await User.findOne({
+          $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+        });
+    
+        if (!foundUser) {
+          return res.status(400).json({ message: 'Cannot find a user with this id!' });
+        }
+    
+        res.json(foundUser);
+      },
+
+    async login({ body }, res) {
+        const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+        if (!user) {
+            return res.status(400).json({ message: "Can't find this user" });
+        }
+
+        const correctPw = await user.isCorrectPassword(body.password);
+
+        if (!correctPw) {
+            return res.status(400).json({ message: 'Wrong password!' });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+    },
+}
